34cd85078a338479958a68a9ec8df3f3ff302aab
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-19 07:10:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let select = document.querySelector('#colorSelect')
+let btn = document.querySelector('input')
+let options = document.querySelectorAll('option')
+
+btn.addEventListener('click',()=>{
+	let currColor = select.value
+	removeItem(currColor)
+})
+
+function removeItem(curColor){
+	options.forEach((op)=>{
+		if(op.textContent == curColor){
+			select.removeChild(op)
+		}
+	})
+}
